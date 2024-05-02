04664fdf3656785d31d89ac0a5e780208591e64d
zigzagdev <msts.oo0131@gmail.com>
2024-05-02 15:29:22 +0900

    refactoring-main-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:todos/screens/home-screen.dart';
 
-void main() {
-  runApp(const TodoApp());
-}
 
-class TodoApp extends StatelessWidget {
-  const TodoApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ToDo App',
-      home: HomeScreen(),
-    );
-  }
-}
