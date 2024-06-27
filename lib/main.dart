fa3f198332ca364b5560b5c064d588ecf8d6acea
zigzagdev <msts.oo0131@gmail.com>
2024-06-27 18:41:23 +0900

    design-pattern-change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,54 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import  'package:todos/Task/add.dart';// Import the add.dart file
 
+void main() {
+  runApp(MyHomePage(title: 'Todo App'));
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> tasks = [];
+
+  void _addTask(String task) {
+    setState(() {
+      tasks.add(task);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tasks[index]),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTask(onTaskAdded: _addTask)),
+          );
+        },
+        tooltip: 'Add Task',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
